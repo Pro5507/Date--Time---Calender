ebd3e88e30269443059aeb03bd5455b22933ff7a
Pro5507 <nhupendrakumar9162@gmail.com>
2024-10-10 18:15:09 +0530

    Date & Time

---
+++ cal.py
@@ -0,0 +1,3 @@
+import calendar
+print(calendar.month(2012,1))
+print(calendar.calendar(2008))
---
+++ dt.py
@@ -0,0 +1,5 @@
+import datetime
+a= datetime.datetime.now()
+print(a)
+b= datetime.date.today()
+print(b)
